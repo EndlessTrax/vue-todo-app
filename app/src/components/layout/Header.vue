120c5ea5ba57ed405ae636d0e3610049744ae12c
Ricky White <rickywhite@outlook.com>
2019-02-17 10:31:49 -0500

    Clean up. Added Heading Style

--- app/src/components/layout/Header.vue
+++ app/src/components/layout/Header.vue
@@ -1,15 +1,24 @@
 <template>
-    <header>
-        <h1>To Do App</h1>
-    </header>
+  <header class="header">
+    <h1>To Do List</h1>
+    <small>with Vue.js</small>
+  </header>
 </template>
 
 <script>
 export default {
-    name: 'Header'
-}
+  name: "Header"
+};
 </script>
 
 <style scoped lang="scss">
-    
+.header {
+  background: #292929;
+  color: white;
+  text-align: center;
+  padding: 25px;
+}
+h1 {
+  font-family: "Lucida Sans", "Verdana", sans-serif;
+}
 </style>
