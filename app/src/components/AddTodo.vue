00c7e45d4f6f7a45a34ea91ee92093e10c98e72e
Ricky White <rickywhite@outlook.com>
2019-02-17 11:06:51 -0500

    Adding more styling

--- app/src/components/AddTodo.vue
+++ app/src/components/AddTodo.vue
@@ -1,39 +1,52 @@
 <template>
-    <div>
-        <form @submit='addTodo'>
-            <input type="text" v-model="title" name="title" placeholder="Add Todo...">
-            <input type="submit" value="Submit" class="submit-btn">
-        </form>
-    </div>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+      <input type="submit" value="Submit" class="submit-btn">
+    </form>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: 'AddTodo',
-    data() {
-        return {
-            title: ''
-        }
-    },
-    methods: {
-        addTodo(e){
-            e.preventDefault();
-            const newTodo = { 
-                title: this.title,
-                completed: false
-            }
-            // Send up to parent
-            this.$emit('add-todo', newTodo);
-            // CLear form
-            this.title = '';
-        }
-    },
-}
+  name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        title: this.title,
+        completed: false
+      };
+      // Send up to parent
+      this.$emit("add-todo", newTodo);
+      // CLear form
+      this.title = "";
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    .submit-btn {
-        background-color: green;
-    }
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 10px;
+}
+input[type="submit"] {
+  flex: 2;
+  padding: 10px;
+}
+.submit-btn {
+  background: greenyellow;
+  border-radius: 5px;
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+}
 </style>
