885e90cb56ca5467ddc2b1f9dc2a9bdc8a70711b
Ricky White <rickywhite@outlook.com>
2019-02-17 09:58:18 -0500

    Done as per Crash Course by Brad Traversy. Still needs styling.

---
+++ app/src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{ todo.title}} 
+            <button @click="$emit('del-todo', todo.id)" class="del"> X </button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"TodoItem",
+    props: ['todo'],
+    methods: {
+        markComplete(){
+            this.todo.completed =!this.todo.completed;
+        }
+    },
+}
+</script>
+
+<style scoped lang="scss">
+    .todo-item {
+        padding: 10px;
+    }
+    .is-complete {
+        text-decoration: line-through;
+    }
+    .del {
+        background-color: red;
+    }
+</style>
---
+++ app/src/components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1>To Do App</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+    
+</style>
